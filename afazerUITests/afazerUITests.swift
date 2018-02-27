b85c591eab2a5c2bcf3bfcee2725ab0a5f476885
Bruno Lemgruber <brunoteixeiralc@gmail.com>
2018-02-27 15:01:51 -0300

    commit - fastlane - aFazer

--- afazerUITests/afazerUITests.swift
+++ afazerUITests/afazerUITests.swift
@@ -26,7 +26,7 @@ class afazerUITests: XCTestCase {
         
         snapshot("Main")
         let app = XCUIApplication()
-        app.navigationBars["Checklists"].buttons["Add"].tap()
+        app.navigationBars["Checklistss"].buttons["Add"].tap()
         snapshot("Add Checklist")
         app.navigationBars["Adicionar checklist"].buttons["Cancelar"].tap()
         let tablesQuery = app.tables
